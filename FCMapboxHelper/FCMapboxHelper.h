a52120bdc9ea26e78e61db3b0f8160f9c2523211
Igor Kalashnikov <zo0m.cfg@gmail.com>
2020-10-16 20:41:16 +0300

    try to move framework

---
+++ FCMapboxHelper/FCMapboxHelper.h
@@ -0,0 +1,16 @@
+//
+//  FCMapboxHelper.h
+//  FCMapboxHelper
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FCMapboxHelper.
+FOUNDATION_EXPORT double FCMapboxHelperVersionNumber;
+
+//! Project version string for FCMapboxHelper.
+FOUNDATION_EXPORT const unsigned char FCMapboxHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mapbox_Tools/PublicHeader.h>
+
+
