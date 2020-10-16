4f73a66a992b6ce299466c746fb531d982c5558c
Igor Kalashnikov <zo0m.cfg@gmail.com>
2020-10-16 16:39:08 +0300

    initial

---
+++ Example/Pods/Target Support Files/FCMapboxHelper/FCMapboxHelper-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FCMapboxHelper : NSObject
+@end
+@implementation PodsDummy_FCMapboxHelper
+@end
---
+++ Example/Pods/Target Support Files/Pods-FCMapboxHelper_Tests/Pods-FCMapboxHelper_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FCMapboxHelper_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_FCMapboxHelper_Tests
+@end
